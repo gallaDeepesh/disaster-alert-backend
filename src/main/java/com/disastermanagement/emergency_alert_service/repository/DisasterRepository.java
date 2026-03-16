7aefdee20fa3083bba0a698f3d27f91abaf96fbc
gallaDeepesh <galladeepesh@gmail.com>
2026-03-16 21:34:37 +0530

    modified DisasterRepository

--- src/main/java/com/disastermanagement/emergency_alert_service/repository/DisasterRepository.java
+++ src/main/java/com/disastermanagement/emergency_alert_service/repository/DisasterRepository.java
@@ -3,8 +3,11 @@ package com.disastermanagement.emergency_alert_service.repository;
 import com.disastermanagement.emergency_alert_service.entity.Disaster;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface DisasterRepository extends JpaRepository<Disaster, Long> {
     Optional<Disaster> findByExternalId(String externalId);
+
+    List<Disaster> findByType(String earthquake);
 }
